af722daccf092158b781483dc324507da3bd4ca9
Say.li <120011676@qq.com>
2023-05-10 19:32:53 +0800

    init

---
+++ src/test/java/com/github/qq120011676/agentserver/AgentServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.qq120011676.agentserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgentServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
