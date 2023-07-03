e4c7d1671a8f81731be0d51b282ffb38ef443803
Say.li <120011676@qq.com>
2023-07-03 18:49:48 +0800

    尝试 解决 代理慢情况

--- src/main/java/com/github/qq120011676/agentserver/controller/AgentController.java
+++ src/main/java/com/github/qq120011676/agentserver/controller/AgentController.java
@@ -1,31 +1,23 @@
 package com.github.qq120011676.agentserver.controller;
 
 import cn.hutool.core.codec.Base64;
-import org.jsoup.Connection;
-import org.jsoup.Jsoup;
+import cn.hutool.http.HttpResponse;
+import cn.hutool.http.HttpUtil;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.io.IOException;
 
 @RestController
 @RequestMapping("/agent")
 public class AgentController {
 
     @RequestMapping("/get")
-    public ResponseEntity<byte[]> url(String url) throws IOException {
+    public ResponseEntity<byte[]> url(String url) {
         String u = Base64.decodeStr(url);
-        Connection connection = Jsoup
-                .connect(u)
-                .maxBodySize(0)
-                .timeout(1000 * 60 * 30);
-        Connection.Response resp = connection
-                .ignoreContentType(true)
-                .ignoreHttpErrors(true)
-                .execute();
-        return ResponseEntity.status(resp.statusCode())
-                .header("Content-Type", resp.contentType())
-                .body(resp.bodyAsBytes());
+        try (HttpResponse httpResponse = HttpUtil.createGet(u).execute()) {
+            return ResponseEntity.status(httpResponse.getStatus())
+                    .header("Content-Type", httpResponse.header("Content-Type"))
+                    .body(httpResponse.bodyBytes());
+        }
     }
 }
